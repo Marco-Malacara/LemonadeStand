9670c673cba86ca1780c8082f01cc179251828a0
Marco-Malacara <marco.malacara7@gmail.com>
2018-08-01 06:26:06 -0500

    wrote a function to get lemons however could not make catch for userInput invalid

--- LemonadeStand/LemonadeStand/Store.cs
+++ LemonadeStand/LemonadeStand/Store.cs
@@ -18,6 +18,28 @@ namespace LemonadeStand
         }
 
         // member methods (CAN DO)
-        
+        public void BuyItems()
+        {
+            Console.WriteLine("Press [1] to buy Cups.");
+            Console.WriteLine("Press [2] to buy Lemons.");
+            Console.WriteLine("Press [3] to buy Sugar.");
+            Console.WriteLine("Press [4[ to buy Ice.");
+
+            string userInput = Console.ReadLine();
+            switch (userInput)
+            {
+                case "1":
+                    GetLemons(player);
+                    break;
+            }
+        }
+        public int AmountOfLemonsNeeded(Player player)
+        {
+            Console.WriteLine("Lemons are $0.15 each.");
+            Console.WriteLine("How many Lemons would you like to buy?");
+
+            int lemonsNeeded = int.Parse(Console.ReadLine());
+            return lemonsNeeded;
+        }
     }
 }
